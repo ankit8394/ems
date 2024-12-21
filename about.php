5cef6f39d9b7abd271820d54040e9c2a8905b0a4
Ankit <ankit0839483@gmail.com>
2024-12-22 00:55:48 +0530

    struct chng

--- about.php
+++ about.php
@@ -1,302 +1,79 @@
+
 <!DOCTYPE html>
-<html>
-
+<html lang="en">
 <head>
-  <!-- Basic -->
-  <meta charset="utf-8" />
-
-
-  <title>Project-EMS</title>
-
-
-  <!-- bootstrap core css -->
-  <link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
-
-  <!-- fonts style -->
-  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700;900&display=swap" rel="stylesheet">
-
-  <!--owl slider stylesheet -->
-  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" />
-
-  <!-- font awesome style -->
-  <link href="css/font-awesome.min.css" rel="stylesheet" />
-
-  <!-- Custom styles for this template -->
-  <link href="css/style.css" rel="stylesheet" />
-  <!-- responsive style -->
-  <link href="css/responsive.css" rel="stylesheet" />
-
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Attendance</title>
+    <link rel="stylesheet" href="index.css">
+    <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
-
-<body class="sub_page">
-
-  <div class="hero_area">
-    <!-- header section strats -->
-    <header class="header_section">
-      <div class="header_top">
-        <div class="container-fluid header_top_container">
-          <div class="lang_box dropdown">
-            <a href="#" title="Language" class="nav-link" data-toggle="dropdown" aria-expanded="true">
-              <img src="images/flag-uk.png" alt="flag" class=" " title="United Kingdom"> <i class="fa fa-angle-down " aria-hidden="true"></i>
-            </a>
-            <div class="dropdown-menu ">
-              <a href="#" class="dropdown-item">
-                <img src="images/flag-france.png" class="" alt="flag">
-              </a>
-            </div>
-            <span>
-              English
-            </span>
-          </div>
-          <div class="contact_nav">
-            <a href="">
-              <i class="fa fa-phone" aria-hidden="true"></i>
-              <span>
-                Call : +91 8394834953
-              </span>
-            </a>
-            <a href="">
-              <i class="fa fa-envelope" aria-hidden="true"></i>
-              <span>
-                Email : ankit0839483@gmail.com
-              </span>
-            </a>
-            <a href="">
-              <i class="fa fa-map-marker" aria-hidden="true"></i>
-              <span>
-              Rishikesh Dehradun India
-              </span>
-            </a>
-          </div>
-          <div class="social_box">
-            <a target="_blank" href="https://github.com/ankit8394">
-              <i class="fa fa-github" aria-hidden="true"></i>
-            </a>
-            <a target="_blank" href="https://www.linkedin.com/in/ankit-5ab596292/">
-              <i class="fa fa-linkedin" aria-hidden="true"></i>
-            </a>
-            <a target="_blank" href="https://www.instagram.com/mr_ankit.0411/profilecard/">
-              <i class="fa fa-instagram" aria-hidden="true"></i>
-            </a>
-          </div>
-        </div>
-      </div>
-      <div class="header_bottom">
-        <div class="container-fluid">
-          <nav class="navbar navbar-expand-lg custom_nav-container ">
-            <a class="navbar-brand" href="#">
-              <img src="assets/logo.jpeg" alt="company-logo" style="height:50px;width:50px">
-            </a>
-
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-              <span class=""> </span>
-            </button>
-
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-              <ul class="navbar-nav  ">
-                <li class="nav-item ">
-                  <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="service.php">Services</a>
-                </li>
-                <li class="nav-item active">
-                  <a class="nav-link" href="about.php"> About</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="project.php">Project</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="testimonial.php">Testimonial</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="contact.php">Contact Us</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="mainlogin.php"> Login</a>
-                </li>
-              </ul>
-            </div>
-          </nav>
-        </div>
-      </div>
-    </header>
-    <!-- end header section -->
-  </div>
-
-  <!-- about section -->
-
-  <section class="about_section layout_padding">
-    <div class="container  ">
-      <div class="row">
-        <div class="col-md-6">
-          <div class="detail-box">
-            <div class="heading_container">
-              <h2>
-                About Us
-              </h2>
-            </div>
-            <p>
-              There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration
-              in some form, by injected humour, or randomised words which don't look even slightly believable. If you
-              are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in
-              the middle of text. All
-            </p>
-            <a href="">
-              Read More
-            </a>
-          </div>
-        </div>
-        <div class="col-md-6 ">
-          <div class="img-box">
-            <img src="images/about-img.jpg" alt="">
-          </div>
-        </div>
-
-      </div>
-    </div>
-  </section>
-
-  <!-- end about section -->
-
-  <!-- info section -->
-  <section class="info_section">
-  <div class="container">
-      <div class="contact_nav">
-        <a href="">
-          <i class="fa fa-phone" aria-hidden="true"></i>
-          <span>
-            Call : +91 8394834953
-          </span>
-        </a>
-        <a href="">
-          <i class="fa fa-envelope" aria-hidden="true"></i>
-          <span>
-            Email : ankit0839483@gmail.com
-          </span>
-        </a>
-        <a href="">
-          <i class="fa fa-map-marker" aria-hidden="true"></i>
-          <span>
-            Rishikesh Dehradun India
-          </span>
-        </a>
-      </div>
-      <div class="info_top ">
-        <div class="row info_main_row">
-          <div class="col-sm-6 col-md-4 col-lg-3">
-            <div class="info_links">
-              <h4>
-                QUICK LINKS
-              </h4>
-              <div class="info_links_menu">
-                <a class="" href="index.php">Home <span class="sr-only">(current)</span></a>
-                <a class="" href="service.php">Services</a>
-                <a class="" href="about.php"> About</a>
-                <a class="" href="project.php">Project</a>
-                <a class="" href="testimonial.php">Testimonial</a>
-                <a class="" href="contact.php">Contact Us</a>
-              </div>
-            </div>
-          </div>
-          <div class="col-sm-6 col-md-4 col-lg-3 mx-auto">
-            <div class="info_post">
-              <h5>
-                INSTAGRAM FEEDS
-              </h5>
-              <div class="post_box">
-                <div class="img-box">
-                  <img src="images/instagram.jpg" alt="">
-                </div>
-                <div class="img-box">
-                  <img src="images/instagram.jpg" alt="">
-                </div>
-                <div class="img-box">
-                  <img src="images/instagram.jpg" alt="">
-                </div>
-                <div class="img-box">
-                  <img src="images/instagram.jpg" alt="">
-                </div>
-                <div class="img-box">
-                  <img src="images/instagram.jpg" alt="">
-                </div>
-                <div class="img-box">
-                  <img src="images/instagram.jpg" alt="">
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="col-md-4">
-            <div class="info_form">
-              <h4>
-                SIGN UP TO OUR NEWSLETTER
-              </h4>
-              <form action="">
-                <input type="text" placeholder="Enter Your Email" />
-                <button type="submit">
-                  Subscribe
-                </button>
-              </form>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="info_bottom">
+<style>
+    <?php include('index.css'); ?>
+</style>
+<body>
+        <?php include('employee-header.php');?>
+    <div class="container-fluid" id="abo">
         <div class="row">
-          <div class="col-md-2">
-            <div class="info_logo">
-            <a href="#">
-                <img src="assets/logo.jpeg" alt="" style="border-radius:50%;height:50px;width:50px">
-              </a>
+            <?php include('employee-sidebar.php');?>
+            <div class="col-sm-12">
+            <div class="row">
+                <div class="col-sm-1">
+                    <div id="btn-mini-div">
+                        <button id="btn-mini"><i class="fa-solid fa-bars"></i></button>
+                    </div>
+                </div>
+                <div class="col-sm-11">
+                <div class="container-fluid" id="extra">
+                    <div class="row">
+                        <div class="col-sm-12" id="about">
+                            <div id="yes">
+                                <h1>Ankit</h1>
+                                <h2>+91 8394834953</h2>
+                                <h2>ankit0839483@gmail.com</h2>
+                                <h2>Rishikesh 249201</h2>
+                            </div>
+                        </div>
+                    </div>
+                    
+                </div>
+</div>
             </div>
-          </div>
-          <div class="col-md-4 ml-auto">
-          <div class="social_box">
-            <a target="_blank" href="https://github.com/ankit8394">
-              <i class="fa fa-github" aria-hidden="true"></i>
-            </a>
-            <a target="_blank" href="https://www.linkedin.com/in/ankit-5ab596292/">
-              <i class="fa fa-linkedin" aria-hidden="true"></i>
-            </a>
-            <a target="_blank" href="https://www.instagram.com/mr_ankit.0411/profilecard/">
-              <i class="fa fa-instagram" aria-hidden="true"></i>
-            </a>
-          </div>
-          </div>
+            </div>
         </div>
-
-      </div>
     </div>
-  </section>
-  <!-- end info_section -->
 
-  <!-- footer section -->
-  <footer class="footer_section">
-    <div class="container">
-      <p>
-        &copy; <span id="displayYear"></span> All Rights Reserved By
-        <a href="#">Developr - <span style="color:#f07b26;font-weight:600">Ankit</span></a>
-      </p>
-    </div>
-  </footer>
-  <!-- footer section -->
+    <?php include('footer.php'); ?>
+    <script>
+$(document).ready(function(){
+    $("#btn-header2").click(function()
+    {
+        $(".two").slideToggle("slow");   
+  });
+    $("#btn-mini").click(function()
+    {
+        $("#hide").fadeIn(1000);
+        $("#btn-mini-div").fadeOut();
+        $("#extra").css("padding-left","150px");
+        $("#extra").css("transition",".75s");
+  });
+    });
+    
+$(document).ready(function(){
+    $("#btn-big").click(function(){
+        $("#hide").fadeOut(1000);
+        $("#btn-mini-div").fadeIn();
+        $("#extra").css("padding-left","10px");
+        $("#extra").css("transition",".75s");
+    });
+ 
+});
 
-  <!-- jQery -->
-  <script src="js/jquery-3.4.1.min.js"></script>
-  <!-- popper js -->
-  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
-  </script>
-  <!-- bootstrap js -->
-  <script src="js/bootstrap.js"></script>
-  <!-- owl slider -->
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js">
-  </script>
-  <!--  OwlCarousel 2 - Filter -->
-  <script src="https://huynhhuynh.github.io/owlcarousel2-filter/dist/owlcarousel2-filter.min.js"></script>
-  <!-- custom js -->
-  <script src="js/custom.js"></script>
+ 
+</script>
 
 
-</body>
-
+    </body>
 </html>
